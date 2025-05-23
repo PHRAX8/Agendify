33fbf3fe9e0eb466f0bf75c075d26250e51113ff
PHRAX8 <lootx8phra@gmail.com>
2025-05-23 11:15:22 -0300

    create screen ui

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -1,10 +1,111 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { View, Text, KeyboardAvoidingView, ScrollView, TextInput, Platform, TouchableOpacity } from 'react-native';
+import styles from "../../assets/styles/create.styles";
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '../../constants/colors';
 
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  //const [date, setDate] = useState(new Date());
+  //const [dateTime, setDateTime] = useState(dateTime);
+  const [price, setPrice] = useState("10.00");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+
+  }
+
   return (
-    <View>
-      <Text>Create Tab</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView contentContainerStyle={styles.container} style={styles.scrollViewStyle}>
+        <View style={styles.card}>
+          {/* Header */}
+          <View style={styles.header}>
+            <Text style={styles.title}>Add Service</Text>
+            <Text style={styles.subtitle}>All your work in one place</Text>
+          </View>
+
+          {/* Title */}
+          <View style={styles.form}>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Title</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="pencil-outline"
+                  size={20}
+                  color={COLORS.textSecondary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter service title"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={title}
+                  onChangeText={setTitle}
+                />
+              </View>
+            </View>
+          </View>
+
+          {/* Price */}
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Your Price</Text>
+            <View style={styles.inputContainer}>
+                <Ionicons
+                  name="cash-outline"
+                  size={20}
+                  color={COLORS.textSecondary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  placeholder="Enter price"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={price}
+                  onChangeText={setPrice}
+                />
+              </View>
+          </View>
+
+          {/* Caption */}
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Caption</Text>
+            <TextInput
+              style={styles.textArea}
+              placeholder="Write your service description here!"
+              placeholderTextColor={COLORS.placeholderText}
+              value={caption}
+              onChangeText={setCaption}
+              multiline
+            />
+          </View>
+
+          {/* Button */}
+          <TouchableOpacity style={styles.button} onPress={handleSubmit} disabled={isLoading}>
+            {isLoading ? (
+              <ActivityIndicator color={COLORS.white} />
+            ) : (
+              <>
+                <Ionicons
+                name="cloud-upload-outline"
+                size={20}
+                color={COLORS.white}
+                style={styles.buttonIcon}
+                />
+                <Text style={styles.buttonText}>Save</Text>
+              </>
+            )}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
